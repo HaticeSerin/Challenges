56f7f6764bb2259b22e4dd0699303c26da4230a0
HaticeSerin <119755031+HaticeSerin@users.noreply.github.com>
2022-12-13 10:02:48 +0300

    Hatice Serin
    @themanoftalent

--- printYourName.c
+++ printYourName.c
@@ -1,1 +1,18 @@
 // your name surname and age from user
+#include"stdio.h"
+#include"stdlib.h"
+#include"string.h"
+int main(){
+char name[25];
+char surname[25];
+int age;
+basla:
+printf("Enter your name and surname:\n");
+scanf("%s%s",name,surname);
+printf("Enter your age:\n");
+scanf("%d",&age);
+goto basla;
+
+return 0;
+
+}
